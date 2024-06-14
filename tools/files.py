0a82f965e268386fa006affd1f652e7542f6d574
Leandro Panesso Atehortúa <leandropa00@gmail.com>
2024-06-14 00:39:51 -0500

    feat: :construction: Get public files with its metadata

---
+++ tools/files.py
@@ -0,0 +1,47 @@
+import subprocess
+import requests
+
+def run_dirb(target_url):
+    try:
+        result = subprocess.run(['dirb', target_url], capture_output=True, text=True)
+
+        if result.returncode == 0:
+            output_lines = result.stdout.splitlines()
+            files_found = [line for line in output_lines if "==> DIRECTORY: " not in line]
+
+            return files_found
+
+        else:
+            return [f"Error running dirb: {result.stderr}"]
+
+    except FileNotFoundError:
+        return ["dirb command not found. Please install dirb."]
+
+    except Exception as e:
+        return [f"Error: {e}"]
+    
+def get_metadata(url):
+    response = requests.head(url)
+
+    if response.status_code == 200:
+        print(response.headers)
+
+def show(http_hosts):
+    print("\n===== PUBLIC FILES ENUMERATION =====\n")
+    
+    print("Active HTTP(S) hosts:")
+    for host in http_hosts:
+        print(f"- {host[0]}:{host[1]}\t{host[2]}")
+    
+    for host in http_hosts:
+        target = f"{host[2]}://{host[0]}:{host[1]}"
+        print(f"\nSearching public files in {target}:")
+        results = run_dirb(target)
+        
+        if results:
+            print("\nFound files:")
+            for line in results:
+                # get_metadata(line)
+                print(line)
+                
+                
