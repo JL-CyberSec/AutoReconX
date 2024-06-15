e0d571a0274c5ec53b8b952bcf83e2210c3549bb
Alejandro Piraquive <alejandro5.6@icloud.com>
2024-06-14 20:22:45 -0500

    Adjust print for metadata

--- tools/files.py
+++ tools/files.py
@@ -68,6 +68,7 @@ def get_metadata(url):
     if response.status_code == 200:
         for metadata in response.headers:
             print(f"\t{metadata}: {response.headers[metadata]}")
+    print('\n')
 
 def show(http_hosts):
     """
